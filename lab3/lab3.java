5791e5bca4235d17d5cc1a544cefe5c7d99082ed
harshithsuresh696-arch <harshithsuresh696@gmail.com>
2026-02-06 10:10:53 +0530

    Update lab3.java

--- lab3/lab3.java
+++ lab3/lab3.java
@@ -1,1 +1,32 @@
+class InsertionSortTrace {
+    public static void main(String[] args) {
+        int[] a = {5, 3, 4, 1, 2};
+        int n = a.length;
+        int i, j, temp;
 
+        System.out.print("Initial: ");
+        printArray(a);
+
+        for (i = 1; i < n; i++) {
+            temp = a[i];
+            j = i - 1;
+
+            while (j >= 0 && a[j] > temp) {
+                a[j + 1] = a[j];
+                j--;
+            }
+
+            a[j + 1] = temp;
+
+            System.out.print("Pass " + i + ": ");
+            printArray(a);
+        }
+    }
+
+    static void printArray(int[] a) {
+        for (int x : a) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+}
